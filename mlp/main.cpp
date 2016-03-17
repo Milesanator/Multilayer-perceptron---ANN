e9e28af8fe566339660ec30ab0e5e5106a14fa8a
Miles Tuffs <milest94@gmail.com>
2016-03-18 10:49:17 +1100

    Initial Commit

---
+++ mlp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mlp
+//
+//  Created by Miles Tuffs on 18/03/2016.
+//  Copyright © 2016 Miles Tuffs. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
